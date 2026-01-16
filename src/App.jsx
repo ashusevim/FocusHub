5f275469de6afbc6fc305c06d7f2f0ff8b9657c7
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2026-01-16 11:20:04 +0530

    Refactor App component to use useReducer for state management and update boardReducer to handle task addition, movement, and deletion

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,44 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import Columns from "./components/board/Columns";
 import NavBar from "./components/layout/NavBar";
 import SideBar from "./components/layout/SideBar";
+import { useReducer } from "react";
+import boardReducer from "./reducers/boardReducer"
+
+const initialState = {
+    columns: {
+        "todo": { id: "todo", title: "To Do", taskIds: [1, 2] },
+        "inprogress": { id: "inprogress", title: "In Progress", taskIds: [3] },
+        "done": { id: "done", title: "Done", taskIds }
+    },
+    tasks: {
+        1: { id: 1, title: "Building kanban board", tags: ["Building", "Kanban"] },
+        2: { id: 2, title: "Learn Docker", tags: ["Docker", "Learning", "containerization"] },
+        3: { id: 1, title: "Preparing for exam", tags: ["Exam"] },
+        4: { id: 1, title: "Apply for an internship", tags: ["Internship"] }
+    }
+}
+
 
 function App() {
-	const todo = [
-		{
-            id: 1,
-			title: "Building kanban board",
-			tags: ["Building", "Software Development", "Kanban"]
-		},
-		{
-            id: 2,
-			title: "Learn Docker",
-			tags: ["Docker", "Learning", "containerization"]
-		}
-	]
-
-    const inprogress = [
-        {
-            id: 1,
-            title: "Preparing for exam",
-            tags: ["Exam Preparation", ""]
-        }
-    ]
-
-    const done = [
-        {
-            id: 1,
-            title: "Apply for an internship",
-            tags: ["Internship", "Job", "Application"]
-        }
-    ]
-	return (
-		<>
-			<NavBar />
-			<Outlet />
+    const [boardState, dispatch] = useReducer(boardReducer, initialState);
+    return (
+        <>
+            <NavBar />
+            <Outlet />
             <div className="flex flex-row space-x-1 p-1 h-screen">
                 <div>
-                    <SideBar/>
+                    <SideBar />
                 </div>
                 <div className="flex flex-1 space-x-2">
                     <Columns title={"To do"} tasks={todo} />
-                    <Columns title={"In Progress"} tasks={inprogress}/>
-                    <Columns title={"Done"} tasks={done}/>
+                    <Columns title={"In Progress"} tasks={inprogress} />
+                    <Columns title={"Done"} tasks={done} />
                 </div>
             </div>
-		</>
-	);
+        </>
+    );
 }
 
 export default App;
--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -1,29 +1,76 @@
 function boardReducer(state, action){
     switch(action.type){
         case 'ADD_TASK': {
+
+            const newTask = {
+                id: action.id,
+                title: action.title,
+                tags: action.tags || []
+            }
+
+            // 1. Add task to task object
+            // 2. Add task Id to specific column's taskIds array
+            const columnId = action.columnId // know which column!
+
             // find column, push new tasks, return new state object
-            return [
+            return {
                 ...state,
-                {
-                    id: action.id,
-                    title: action.title,
-                    tags: action.tags
+                tasks: {
+                    ...state.tasks,
+                    [newTask.id]: newTask
+                },
+                columns: {
+                    ...state.columns,
+                    [columnId]: {
+                        ...state.columns[columnId],
+                        taskIds: [...state.columns[columnId].taskIds, newTask.id]
+                    }
                 }
-            ]
+            };
         }
 
         case 'MOVE_TASK': {
-
+            const { taskId, sourceColumnId, targetColumnId } = action;
+            // filter out from the source column and push to target column
+            return {
+                ...state,
+                columns: {
+                    ...state.columns,
+                    [sourceColumnId]: {
+                        ...state.columns[sourceColumnId],
+                        taskIds: state.columns[sourceColumnId].taskIds.filter(id=>id !== taskId)
+                    },
+                    [targetColumnId]: {
+                        ...state.columns[targetColumnId],
+                        taskIds: [...state.columns[targetColumnId].taskIds,  taskId]
+                    }
+                }
+            }
         }
 
         // Return a new array with given task excluded
         case 'DELETE_TASK': {
-            return state.filter((t)=> t.id !== action.id)
-        }
+            const { taskId, columnId } = action;
 
-        case 'ADD_COLUMN': {
-            
+            // remove the task from the task object
+            const newTasks = {...state.tasks};
+            delete newTasks[taskId];
+
+            return {
+                ...state,
+                tasks: newTasks,
+                columns: {
+                    ...state.columns[columnId],
+                    [columnId]: {
+                        ...state.columns[columnId],
+                        // filter out the taskId from the column
+                        taskIds: state.columns[columnId].taskIds.filter(id=>id!==taskId)
+                    }
+                }
+            }
         }
+        default:
+            return state;
     }
 }
 
