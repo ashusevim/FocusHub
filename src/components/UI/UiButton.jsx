4da8d50abbb8ee29a90743881efe7c9f7d95b3d0
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2026-02-04 22:46:18 +0530

    FIX: minor bug fix in button component

--- src/components/UI/UiButton.jsx
+++ src/components/UI/UiButton.jsx
@@ -1,8 +1,8 @@
-function UiButton({text, handler}){
+function UiButton({text, onClick}){
     return (
         <>
             <button 
-                onClick={handler}
+                onClick={onClick}
                 className="px-2 py-2 border rounded-md font-medium transition-colors flex items-center bg-slate-900 text-white hover:cursor-pointer
             justify-center w-24">
                 {text}
