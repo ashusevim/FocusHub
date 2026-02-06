ad170e13e6619469a4d6cf60f31ee2e976eb8544
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2026-02-06 23:09:14 +0530

    feat: enhance modal styles and layout in TaskModal component for better user experience

--- src/components/modal/TaskModal.jsx
+++ src/components/modal/TaskModal.jsx
@@ -32,31 +32,33 @@ export default function TaskModal({ task, columnId, onClose }){
 
     return createPortal(
         <div className="modal-overlay" onClick={onClose}>
-            <div className="modal-content" onClick={(e)=>e.stopPropagation()}>
-                <h2>Edit Task</h2>
-                <label>
-                    Title:
+            <div className="modal-content flex flex-col w-10 justify-evenly" onClick={(e)=>e.stopPropagation()}>
+                <h2 className="text-3xl mb-3 text-foreground">Edit Task</h2>
+                <label className="flex items-center mb-2 gap-2 text-foreground">
+                    <h2>Title:</h2>
                     <input
                         type="text"
                         value={title}
+                        className="flex h-9 w-full rounded-lg border border-input bg-background px-3 py-2 text-sm text-foreground shadow-sm shadow-black/5 transition-shadow placeholder:text-muted-foreground/70 focus-visible:border-ring focus-visible:outline-none focus-visible:ring-[3px] focus-visible:ring-ring/20 disabled:cursor-not-allowed disabled:opacity-50"
                         onChange={(e) => {
                             setTitle(e.target.value)
                         }}
                     />
                 </label>
-                <label>
+                <label className="flex items-center mb-2 gap-2">
                     Tags (comma-separated):
                     <input
                         type="text"
                         value={tags}
+                        className="flex h-9 w-full rounded-lg border border-input bg-background px-3 py-2 text-sm text-foreground shadow-sm shadow-black/5 transition-shadow placeholder:text-muted-foreground/70 focus-visible:border-ring focus-visible:outline-none focus-visible:ring-[3px] focus-visible:ring-ring/20 disabled:cursor-not-allowed disabled:opacity-50"
                         onChange={(e) => {
                             setTitle(e.target.value)
                         }}
                     />
                 </label>
-                <div className="modal-buttons">
-                        <button onClick={handleSave}>Save</button>
-                        <button onClick={handleCancel}>Cancel</button>
+                <div className="modal-buttons flex gap-2 mt-7">
+                    <button onClick={handleSave} className="inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors outline-offset-2 focus-visible:outline-2 focus-visible:outline-ring/70 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow-sm shadow-black/5 hover:bg-primary/90 h-9 px-4 py-2">Save</button>
+                    <button onClick={handleCancel} className="inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors outline-offset-2 focus-visible:outline-2 focus-visible:outline-ring/70 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm shadow-black/5 hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2">Cancel</button>
                 </div>
             </div>
         </div>,
