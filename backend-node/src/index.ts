92baa7473a1333f87b7b305d55d3f2406fa9c009
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2026-02-22 17:49:50 +0530

    FIXED: minor login and register bugs

--- backend-node/src/index.ts
+++ backend-node/src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { DBConnect } from "./db/db.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { isValidEmail, isValidUsername } from "./utils/inputValidation.js";
 import { User } from "./schema/user.models.js";
 import bcrypt from "bcrypt";
+import cors from "cors";
+import jwt from "jsonwebtoken";
 
 dotenv.config({
-    path: "./.env"
-})
+    path: "./.env",
+});
 
 DBConnect();
 
@@ -15,6 +17,7 @@ const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cors());
 
 /**
  * Register a new user
@@ -25,42 +28,42 @@ app.post("/register", async (req, res) => {
     const { username, email, password } = req.body;
 
     // Validate input fields
-    if(!isValidEmail(email) || !isValidUsername(username) || !password){
+    if (!isValidEmail(email) || !isValidUsername(username) || !password) {
         return res.status(400).json({
-            message: "All fields are required"
-        })
+            message: "All fields are required",
+        });
     }
 
     // check if user with same username or email already exists
     const existedUser = await User.findOne({
-        $or: [{ username }, { email }]
-    })
+        $or: [{ username }, { email }],
+    });
 
-    if(existedUser){
+    if (existedUser) {
         return res.status(409).json({
-            message: "User with same username or email already exists"
-        })
+            message: "User with same username or email already exists",
+        });
     }
 
     // create new user in database
     const user = await User.create({
         username,
         email,
-        password
-    })
+        password,
+    });
 
-    if(!user){
+    if (!user) {
         res.status(409).json({
-            message: "Something went wrong while creating user"
+            message: "Something went wrong while creating user",
         });
         return;
     }
 
     return res.status(201).json({
         statusCode: 201,
-        message: "User Registered successfully"
-    })
-})
+        message: "User Registered successfully",
+    });
+});
 
 /**
  * Login a new user
@@ -70,37 +73,37 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
     const { email, password } = req.body;
 
-    if(!isValidEmail(email) || !password){
+    if (!isValidEmail(email) || !password) {
         return res.status(400).json({
-            message: "All fields are required"
-        })
+            message: "All fields are required",
+        });
     }
 
     // check if user with provided email exists in database
-    const user = await User.findOne({ email })
+    const user = await User.findOne({ email });
 
-    if(!user){
+    if (!user) {
         return res.status(400).json({
-            message: "User not found"
-        })
+            message: "User not found",
+        });
     }
 
     // if the user is found, compare the provided password with the stored hashed password
-    if(user.password != password){
+    if (user.password != password) {
         return res.status(401).json({
-            message: "Password is not valid"
-        })
+            message: "Password is not valid",
+        });
     }
 
     return res.status(200).json({
-        message: "User logged in successfully"
-    })
-})
+        message: "User logged in successfully",
+    });
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
-})
+});
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
-})
+});
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,37 +1,6 @@
-import { Button } from "@/components/ui/button";
 import { LoginForm } from "@/components/login-form";
 
-function LoginPage() {
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        
-        try{
-            // Get the username and password from the input fields
-            const username = document.getElementById("username").value;
-            const password = document.getElementById("password").value;
-
-            const response = await fetch("http://localhost:3000/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ username, password })
-            });
-
-            if(!response.ok){
-                console.error("Login failed: ", response.statusText);
-            }
-            else{
-                console.log("Login successful!");
-            }
-        }catch(error){
-            console.error("Login failed:", error);
-        }
-        
-    }
-    return (
-        <LoginForm onSubmit={handleLogin}/>
-    );
+export default function LoginPage() {
+    return <LoginForm />;
 }
 
-export default LoginPage;
